5b2b8577b37416de25d0d3070220821cd801029c
kzcheng <kevinzifancheng@gmail.com>
2025-01-30 13:17:41 -0800

    Q8 completed
    I have no idea what I did. I'm gonna try to refactor it a bit.

--- searchAgents.py
+++ searchAgents.py
@@ -657,8 +657,71 @@ class ClosestDotSearchAgent(SearchAgent):
         walls = gameState.getWalls()
         problem = AnyFoodSearchProblem(gameState)
 
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # logging.getLogger().setLevel(logging.DEBUG)
+
+        # Q8: Suboptimal Search
+
+        # To find the path to the closest dot, use a BFS
+
+        # The fringe online stores 1 thing, which is the state we are going towards
+        fringe = util.Queue()
+
+        # Each element records the path to reach each state
+        # In BFS, only the first way of reaching each state is recorded
+        paths = {}
+
+        state = problem.getStartState()
+        path = []   # The path is the path to reach the current state from start
+
+        # Stuff used for debugging
+        stepCounter = 0
+
+        # Before we begin looping through the fringe to analyze every state, we need to add the initial condition into the fringe
+        fringe.push(state)
+        paths[state] = path.copy()
+
+        while stepCounter < 1000000:  # Arbitrary cap to number of loops
+            if fringe.isEmpty():
+                logging.info("\nThe fringe is empty")
+                logging.info("No solution found")
+                return []
+
+            # If there are still things we can analyze in the fringe, we should do it
+            stepCounter += 1
+            logging.debug("\n\n[ Step %r ]", stepCounter)
+            # logging.debug("Current entire fringe: %r", fringe.list)
+            logging.debug("\nThe paths to reach every state: %r", paths)
+
+            # Popping the fringe to decide what is the next state we need to visit, which we will then go to it
+            state = fringe.pop()
+            path = paths[state].copy()
+            logging.debug("Current state we are analyzing: %r", state)
+
+            # Check if we are analyzing the goal, if so, we are done
+            if problem.isGoalState(state):
+                logging.debug("")
+                logging.info("Goal State Reached")
+
+                logging.debug("Path Found")
+                logging.debug("Final Path: %r", path)
+                return path
+
+            # If this is not the goal, we must expand it
+            for successor in problem.getSuccessors(state):
+                nextState, nextAction, _ = successor
+                nextPath = path + [nextAction]
+                if nextState not in paths:
+                    fringe.push(nextState)
+                    logging.debug("Added new state to fringe: %r", nextState)
+                    paths[nextState] = nextPath.copy()
+                    logging.debug("With planned path to reach state: %r", nextPath)
+
+            # logging.debug("Current entire fringe: %r", fringe.list)
+
+        logging.error("Loop limit reached, aborting search")
+
+        logging.getLogger().setLevel(logging.INFO)
+        return []
 
 
 class AnyFoodSearchProblem(PositionSearchProblem):
@@ -694,8 +757,9 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         """
         x, y = state
 
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        if state in self.food.asList():
+            return True
+        return False
 
 
 def mazeDistance(point1: Tuple[int, int], point2: Tuple[int, int], gameState: pacman.GameState) -> int:
